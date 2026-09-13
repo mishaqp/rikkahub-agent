249bb90d29965907015f3d896bef9c9988bf8df6
mishaqp <84035331+mishaqp@users.noreply.github.com>
2026-09-13 11:55:58 +0300

    chore(release): bump version to 2.5.1-agent.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
         minSdk = 26
         targetSdk = 37
             versionCode = releaseVersionCode
-            versionName = "2.5.1-agent.2"
+            versionName = "2.5.1-agent.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
